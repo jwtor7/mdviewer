3cfee611dda2797bcdec31af0e2558a8a2065ac6
Junior <jr@trustcyber.ca>
2025-11-21 00:06:57 -0500

    Configure strict CSP and Electron security settings

--- src/main.js
+++ src/main.js
@@ -14,6 +14,9 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
     },
   });
 
